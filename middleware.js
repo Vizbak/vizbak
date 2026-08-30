76c89d5963e7954868096708a140776b261d9036
Vizbak <ani36nc@gmail.com>
2026-08-31 04:34:44 +0530

    Route desktop production to final VIZBAK experience

--- middleware.js
+++ middleware.js
@@ -1,14 +1,9 @@
 import { rewrite } from '@vercel/functions';
 
-export const config = {
-  matcher: '/',
-};
+export const config = { matcher: '/', };
 
 export default function middleware(request) {
   const userAgent = request.headers.get('user-agent') || '';
   const isMobile = /android|iphone|ipad|ipod|mobile/i.test(userAgent);
-
-  return rewrite(
-    new URL(isMobile ? '/mobile.html' : '/desktop-v3.html', request.url)
-  );
+  return rewrite(new URL(isMobile ? '/mobile.html' : '/desktop-final.html', request.url));
 }
