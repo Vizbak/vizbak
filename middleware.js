e0d2c41413eb797cc4dca24121c412c7b2bdca61
Vizbak <ani36nc@gmail.com>
2026-08-31 05:14:44 +0530

    Route mobile visitors to rebuilt mobile experience

--- middleware.js
+++ middleware.js
@@ -5,5 +5,5 @@ export const config = { matcher: '/', };
 export default function middleware(request) {
   const userAgent = request.headers.get('user-agent') || '';
   const isMobile = /android|iphone|ipad|ipod|mobile/i.test(userAgent);
-  return rewrite(new URL(isMobile ? '/mobile.html' : '/desktop-final.html', request.url));
+  return rewrite(new URL(isMobile ? '/mobile-v2.html' : '/desktop-final.html', request.url));
 }
